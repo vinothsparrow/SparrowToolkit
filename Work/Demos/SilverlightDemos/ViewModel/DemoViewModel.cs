86917d8e367e9598055d6b6c3c0565df2ee860cd
vinothsparrow_cp <SND\vinothsparrow_cp>
2013-05-08 13:00:18 +0000

    Silverlight Demos Added

---
+++ Work/Demos/SilverlightDemos/ViewModel/DemoViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Sparrow.Chart.Demos
+{
+    public class DemoViewModel
+    {       
+        
+        public ObservableCollection<SampleModel> Samples 
+        {
+            get;
+            set; 
+        }               
+
+        public DemoViewModel()
+        {
+           Samples=new ObservableCollection<SampleModel>();
+            AddSamples();
+            
+        }
+
+        private void AddSamples()
+        {
+            //CPUPerformance.CPUView cpuView = new CPUPerformance.CPUView();
+            
+            //Sparrow.Chart.Demos.Demos.PerformanceDemo.PerformanceDemo performanceView = new Demos.PerformanceDemo.PerformanceDemo();
+            SampleModel performanceDemo = new SampleModel("Performance Demo", "", "Sparrow.Chart.Demos.Demos.PerformanceDemo.PerformanceDemo");
+            performanceDemo.IsHeader = false;
+
+            SampleModel liveDataDemo = new SampleModel("Live Datas Demo", "", "Sparrow.Chart.Demos.Demos.LiveDatasDemo.LiveDatasDemo");
+            liveDataDemo.IsHeader = false;
+            
+            Samples.Add(liveDataDemo);
+            Samples.Add(performanceDemo);
+         
+        }
+
+       
+    }
+}
